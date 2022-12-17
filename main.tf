11ed4ee0007cc33f1cd188b11d810b5f66afcef1
chinedum <ezechinedum504@gmail.com>
2022-12-17 11:49:49 +0100

    adding SecurityGroup  and Route Table

--- main.tf
+++ main.tf
@@ -6,14 +6,12 @@ provider "aws" {
   # Configuration options
 }
 
-variable "aws_vpc_name" {
-  description = "this is the name of the VPC" 
-  default = "nedum-dev-vpc" 
+variable "name" {
+  description = "this is the name of the VPC/Subnet" 
 }
 
-variable "aws_subnet_name" {
-  description = "this is the name of the Subnet" 
-  default = "nedum-dev-subnet" 
+variable "rtb-id" {
+  description = "this is the default route table ID" 
 }
 
 variable "cidr_block" {
@@ -21,10 +19,14 @@ variable "cidr_block" {
   type = list(string)
 }
 
+variable "my-IP" {}
+
+
+
 resource "aws_vpc" "nedum_vpc" {
   cidr_block = var.cidr_block[0]
   tags = {
-    "Name" = var.aws_vpc_name 
+    "Name" = "${var.name}-vpc"
   }
 }
 
@@ -33,27 +35,92 @@ resource "aws_subnet" "nedum_subnet" {
  cidr_block = var.cidr_block[1]
  availability_zone = "us-east-1a"
  tags = {
-    "Name" = var.aws_subnet_name 
+    "Name" = "${var.name}-subnet-1" 
   }
 }
 
-data "aws_vpc" "selected" {
-  id = "vpc-03131b6ee59b88afe"
-}
+resource "aws_internet_gateway" "nedum-igw" {
+  vpc_id = aws_vpc.nedum_vpc.id 
 
-resource "aws_subnet" "example" {
-  vpc_id            = data.aws_vpc.selected.id
-  availability_zone = "us-east-1a"
-  cidr_block        = "172.31.48.0/20"
   tags = {
-    Name ="nedum"
+    Name = "${var.name}-igw" 
   }
 }
 
-output "default-vpc-name" {
-  value = aws_subnet.example.tags.Name
+/* // Incase you want to create a new route table aside the defualt rt created by terraform use this below:
+
+resource "aws_route_table" "my-rtb" {
+  vpc_id = aws_vpc.nedum_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nedum-igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-rtb" 
+  }
 }
 
-output "default-vpc-id" {
-  value = aws_subnet.example.vpc_id
+
+
+resource "aws_route_table_association" "rtb-to-subnet-association" {
+  subnet_id      = aws_subnet.nedum_subnet.id
+  route_table_id = aws_route_table.my-rtb.id 
+}
+*/
+
+resource "aws_default_route_table" "my-rtb" {
+  default_route_table_id = aws_vpc.nedum_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nedum-igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-rtb"
+  }
+}// we dont have to explicitely associate the route table to the subnet as all subnet within the VPC will automatically be assigned the route table unless explicitly assigned
+
+// Creating a Security Group
+resource "aws_default_security_group" "my-default-SG" {
+  vpc_id      = aws_vpc.nedum_vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.my-IP]
+    
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-SecurityGroup"
+  }
+}
+
+output "vpc-name" {
+  value = aws_vpc.nedum_vpc.tags.Name
+ 
+}
+
+output "vpc-id" {
+  value = aws_vpc.nedum_vpc.id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
-aws_vpc_name = "nedum-test-vpc"
-aws_subnet_name = "nedum-test-subnet" 
+name = "nedum-aws"
 cidr_block = ["10.0.0.0/16","10.0.20.0/24"]
+rtb-id = "rtb-0b3790c000bd01220"
+my-IP = "102.89.32.99/32"
